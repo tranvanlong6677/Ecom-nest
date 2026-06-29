80eebf236fdb86fd86cb55c1371acadaaa3a6013
Long Tran <tranvanlong6677@gmail.com>
2026-06-29 13:57:59 +0700

    setup db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from './shared/services/prisma.service'
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!'
+  }
+
+  createLanguage() {
+    return this.prisma.language.create({
+      data: {
+        id: 'vi1',
+        name: 'Vietnamese1111',
+      },
+    })
   }
 }
--- src/shared/services/prisma.service.ts
+++ src/shared/services/prisma.service.ts
@@ -1,17 +1,27 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma/client'
+import envConfig from '../config'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService implements OnModuleInit {
   constructor() {
-    const url = process.env.DATABASE_URL
-    if (!url) throw new Error('DATABASE_URL is not defined')
-    const adapter = new PrismaPg({ connectionString: url })
-    super({ adapter, log: ['info'] })
+    const adapter = new PrismaPg({ connectionString: envConfig.DATABASE_URL })
+    const client = new (PrismaClient as any)({ adapter, log: ['info'] })
+
+    return new Proxy(this, {
+      get(target, prop, receiver) {
+        if (prop in target) return Reflect.get(target, prop, receiver)
+        const value = client[prop]
+        return typeof value === 'function' ? value.bind(client) : value
+      },
+    })
   }
 
   async onModuleInit() {
-    await this.$connect()
+    await (this as any).$connect()
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface PrismaService extends PrismaClient {}
